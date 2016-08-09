16b71b347f89f37303b08ebb9e8ca0f6dd917b4e
yuchen <as22877566@yahoo.com.tw>
2016-08-09 07:19:03 +0000

    修改core/App.php, core/Connect.php, core/Controller.php排版

--- core/Controller.php
+++ core/Controller.php
@@ -1,14 +1,15 @@
 <?php
 
-class Controller {
-    public function model($model) {
+class Controller
+{
+    public function model($model)
+    {
         require_once "../EasyMVC/models/$model.php";
         return new $model ();
     }
     
-    public function view($view, $data = Array()) {
+    public function view($view, $data = Array())
+    {
         require_once "../EasyMVC/views/$view.php";
     }
 }
-
-?>
