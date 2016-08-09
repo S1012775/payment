82473b70593f40398c8638bee85b6a0f55605ba9
yuchen <as22877566@yahoo.com.tw>
2016-08-09 09:40:12 +0000

    修改排版及修改變數變駝峰式寫法

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -4,29 +4,28 @@ class HomeController extends Controller
 {
     function item()
     {
-        $browseitem = $this->model("User");
-        // 顯示明細項目
-        $item = $browseitem->item();
-        //顯示餘額
-        $balance = $browseitem->showBalance();
+        $browseItem = $this->model("User");
+        $item = $browseItem->item();
+        $balance = $browseItem->showBalance();
         $data[] = $item;
         $data[] = $balance;
 
-            //收入
+        //收入
         if (isset($_POST['addincome'])) {
-            $incomemoney = $_POST['incomemoney'];
-            $countbalance = $browseitem->countIncome($incomemoney, $balanceNum);
-            $this->view("echo", $countbalance);
+            $incomeMoney = $_POST['incomemoney'];
+            $countBalance = $browseItem->countIncome($incomeMoney, $balanceNum);
+            $this->view("echo", $countBalance);
             header("location:/EasyMVC/Home/item");
         }
-            //支出
+
+        //支出
         if (isset($_POST['expend'])) {
-            $expendmoney = $_POST['expendmoney'];
-            $countexpend=$browseitem->countExpend($balanceNum, $expendmoney);
-            $this->view("echo", $countexpend);
+            $expendMoney = $_POST['expendmoney'];
+            $countExpend = $browseItem->countExpend($balanceNum, $expendmoney);
+            $this->view("echo", $countExpend);
             header("location:/EasyMVC/Home/item");
         }
+
         $this->view("index", $data);
     }
-
 }
