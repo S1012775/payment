e74ae9c148f2c927329056775d494d4a72f3d074
yuchen <as22877566@yahoo.com.tw>
2016-08-09 08:28:40 +0000

    檔名變更

--- core/Controller.php
+++ core/Controller.php
@@ -4,12 +4,12 @@ class Controller
 {
     public function model($model)
     {
-        require_once "../EasyMVC/models/$model.php";
+        require_once "../bankSystem/models/$model.php";
         return new $model ();
     }
 
     public function view($view, $data = Array())
     {
-        require_once "../EasyMVC/views/$view.php";
+        require_once "../bankSystem/views/$view.php";
     }
 }
