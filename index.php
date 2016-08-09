df317a45052253b6fc39f9d55730d627d0e42abb
yuchen <as22877566@yahoo.com.tw>
2016-08-09 09:04:13 +0000

    修改'?>'排版及刪除'ut-f8'程式碼

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-mb_internal_encoding('utf-8');
 require_once 'core/Connect.php';
-#===============================================
+
 require_once 'core/App.php';
+
 require_once 'core/Controller.php';
 
 $app = new App();
 
-?>
+
